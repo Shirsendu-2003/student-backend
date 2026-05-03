752d98232d8a423c53f60ddcb0cd219192c2d568
Shirsendu Pramanik <pramanikshirsendu07@gmail.com>
2026-05-04 01:16:47 +0530

    Update CORS origin for AdminCellController

--- src/main/java/com/studentform/controller/AdminCellController.java
+++ src/main/java/com/studentform/controller/AdminCellController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
  */
 @RestController
 @RequestMapping("/api/admincell")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://admin-view-three.vercel.app/")
 public class AdminCellController {
 
     @Autowired
